267fdf2061f56d2ec53768b53fac75c1e81ee6cc
ATTOLOU Dylan <dylanattolou2005@gmail.com>
2025-07-11 09:20:26 +0100

    Ajout de .gitignore pour ignorer .env, venv/ et caches

--- app/models/mail.py
+++ app/models/mail.py
@@ -15,3 +15,28 @@ def envoyer_email(destinataire, sujet, contenu):
         print("✅ Email envoyé à", destinataire)
     except Exception as e:
         print("❌ Échec d’envoi :", e)
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+
+# Charger les variables du fichier .env
+load_dotenv()
+
+def envoyer_email(destinataire, sujet, contenu):
+    email_expediteur = os.getenv("MAIL_USER")
+    mot_de_passe     = os.getenv("MAIL_PASSWORD")
+
+    msg = EmailMessage()
+    msg['Subject'] = sujet
+    msg['From'] = email_expediteur
+    msg['To'] = destinataire
+    msg.set_content(contenu)
+
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(email_expediteur, mot_de_passe)
+            smtp.send_message(msg)
+        print("✅ Email envoyé à", destinataire)
+    except Exception as e:
+        print("❌ Échec d’envoi :", e)
